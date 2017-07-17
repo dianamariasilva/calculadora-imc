6eb0d73052c87dbbca4351d120cecab1aacc4e06
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-07-17 14:20:35 -0500

    Archivo html modificado, mayuscula Imc en js

--- js/archivo.js
+++ js/archivo.js
@@ -1,12 +1,13 @@
 function indiceMasa(){
-	var peso = parseInt(prompt("indique su masa "));
-	var estatura = parseInt(prompt("indique su estatura en centimetros OJO CENTIMETROS"));
+
+	var peso = parseInt(prompt("Indique su masa "));
+	var estatura = parseInt(prompt("Indique su estatura en centimetros OJO CENTIMETROS"));
 	var altura=estatura/100;
 
 
-	var IM =peso/Math.pow(altura,2);
-	var rpta = IM.toFixed(2);
-	alert(rpta); 
-	
+	var IMC =peso/Math.pow(altura,2);
+	var rpta = IMC.toFixed(2);
+	alert(rpta); 	
+
 }
 indiceMasa();
